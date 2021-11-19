4ee199496d3eec0fc57724b2aa83ae07748dcdf7
unknown <matekoa@gmail.com>
2021-11-19 21:10:34 +0200

    Added comments to files

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//First file that gets loaded in Vue.JS
 
-createApp(App).mount('#app')
+import { createApp } from 'vue' //Loads vue Library
+import App from './App.vue'//loads vue component
+
+createApp(App).mount('#app') //renders everything into an element with an ID of app.
